fb66d3d4d42ee9c01d29dafa7d4eef2908f19c26
pixelsuchter <76219112+pixelsuchter@users.noreply.github.com>
2021-04-03 20:32:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1>Bantool 2</h1>
+
+<h3>New:</h3>
+<ul>
+<li>Doesn't need a focused window anymore</li>
+<li>Controls its own version of Firefox with multiple instances running in parallel</li>
+<li>Keeps track of banned users per channel</li>
+<li>You can now set multiple channels to run on</li>
+</ul>
+
+<h3>How to use:</h3>
+<ol>
+<li>Unzip - Bantool-Firefox.7z</li>
+<li>navigate to and open \Bantool-Firefox\FirefoxPortable\App\Firefox64\Firefox.exe</li>
+<li>type about:profiles into the URL tab</li>
+<li>create a new profile called "bantool" save it in the bantool folder \Bantool-Firefox\ <br>
+	If it doesn't create a proper profile folder (looks like ugcmlygs.bantool) create one yourself</li>
+<li>open bantool profile new window and if needed, set as default
+<li>close and reopen firefox
+<li>log into Twitch
+<li>close out of firefox again
+<li>click main.exe or main.py (python 3.8 or newer needs to be installed for the .py file)
+<li>It will generate the necessary files and will ask you to press enter to close it
+<li><h4>edit the config file:</h4>
+<ul>
+<li>"twitch_channels": ["NAME OF CHANNEL", "NAME OF SECOND CHANNEL", "NAME OF THIRD CHANNEL"],</li>
+<li>"command": "/ban",</li>
+<li>"Number_of_browser_windows": 1,</li>
+<li>"Firefox_profile": "NAME OF PROFILE FOLDER"</li>
+</ul></li>
+
+<li>The amount of browser windows you should put set is dependent on your computer,<br>
+i suggest starting with 1 and working your way up (I can run 8 on a Ryzen 7 3700x)</li>
+<li>After filling the namelist and setting the config you can now open the main.exe or main.py again</li>
+</ol>
---
+++ main.py
@@ -0,0 +1,170 @@
+import datetime
+import json
+import os
+import subprocess
+import sys
+import time
+import _thread
+import glob
+
+try:
+    from selenium.common.exceptions import *
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+    from selenium.webdriver.support.expected_conditions import presence_of_element_located
+except ImportError:
+    # installs selenium module if it is missing, needed for browser control
+    print('Installing selenium')
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "selenium"])
+    from selenium.common.exceptions import *
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+    from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+channels = [""]
+command = None
+num_windows = 0
+
+try:
+    with open("config.json", "r") as cfg:
+        config = json.load(cfg)
+        assert type(config["twitch_channels"]) == list
+        assert type(config["command"]) == str
+        assert type(config["Number_of_browser_windows"]) == int
+        assert type(config["Firefox_profile"]) == str
+    channels = config["twitch_channels"]
+    command = config["command"]
+    num_windows = config["Number_of_browser_windows"]
+except (OSError, json.JSONDecodeError, AssertionError, KeyError) as e:
+    print("Config file does not exist or corrupt, creating default config\n")
+    if os.path.isfile("config.json"):
+        if os.path.isfile("config.json.broken"):
+            os.remove("config.json.broken")
+        os.rename("config.json", "config.json.broken")
+    with open("config.json", "w") as f:
+        config = {"twitch_channels": [""], "command": "/ban", "Number_of_browser_windows": 1, "Firefox_profile": ""}
+        json.dump(config, f)
+
+
+def check_files():
+    if not os.path.isfile("namelist.txt"):
+        print("Namelist does not exist, creating file. Please insert names and restart")
+        with open("namelist.txt", "x"):
+            pass
+        input("Press enter to exit")
+        exit(0)
+
+    if channels == [""] or not command or not num_windows or not config["Firefox_profile"]:
+        print("Config not set correctly")
+        input("Press enter to exit")
+        exit(0)
+
+
+def delete_split_namelists():
+    namelist_files = glob.glob("namelist_split*.txt")
+    for filePath in namelist_files:
+        try:
+            os.remove(filePath)
+        except OSError:
+            print("Error while deleting file : ", filePath)
+
+
+def split_files(channel):
+    split_namelists = []
+    with open("namelist.txt", "r") as namelist:
+        name_count = len(namelist.readlines())
+        namelist.seek(0)
+        if not os.path.isdir("banned_lists"):  # create folder if nescessary
+            os.mkdir("banned_lists")
+        if not os.path.isfile("banned_lists/{streamer}.txt".format(streamer=channel)):  # create streamer specific file if non existant
+            with open("banned_lists/{streamer}.txt".format(streamer=channel), "x"):
+                pass
+        delete_split_namelists()
+        for i in range(num_windows):
+            f = open("namelist_split{num}.txt".format(num=i), "w")
+            split_namelists.append(f)
+        with open("banned_lists/{streamer}.txt".format(streamer=channel), "r") as banned_names:
+            _banned_names = sorted(banned_names.readlines())
+            index = 0
+            temp_count = 0
+            for name in namelist:
+                print("removing already banned users: {} name(s) left".format(name_count-temp_count))
+                banned_names.seek(0)
+                if name in _banned_names:
+                    temp_count += 1
+                    continue
+                else:
+                    split_namelists[index % num_windows].write(name)
+                    index += 1
+                    temp_count += 1
+
+
+def browser(userlist, index, channel):
+    global counter, done
+    try:
+        with open(userlist, "r") as _namelist, open("banned_part{index}.txt".format(index=index), "w+") as banned_names:
+            if len(_namelist.readlines()) > 1:
+                _namelist.seek(0)
+                with webdriver.Firefox(executable_path="FirefoxPortable/App/Firefox64/geckodriver.exe", firefox_profile=config["Firefox_profile"],
+                                       firefox_binary="FirefoxPortable/App/Firefox64/firefox.exe") as driver:
+                    print(driver.profile.profile_dir)
+                    driver.set_window_size(500, 500)
+                    wait = WebDriverWait(driver, 10)
+                    driver.get("https://www.twitch.tv/popout/{channel}/chat".format(channel=channel))
+                    chat_field = wait.until(presence_of_element_located((By.CSS_SELECTOR, ".ScInputBase-sc-1wz0osy-0")))
+                    if chat_field.is_displayed():
+                        chat_field.click()
+                    try:  # remove rules window
+                        rules_button = driver.find_element(By.CSS_SELECTOR, ".dhNyXR")
+                        if rules_button.is_displayed():
+                            rules_button.click()
+                    except NoSuchElementException:
+                        pass
+                    time.sleep(15)
+                    for _name in _namelist:
+                        chat_field.send_keys("{cmd} {name}".format(cmd=command, name=_name), Keys.ENTER)
+                        banned_names.write(_name)
+                        counter[index] += 1
+    except LookupError:
+        print("couldn't start instance {}".format(index))
+    finally:
+        done[index] = True
+
+
+def start_browsers(channel):
+    num_names = 0
+    split_banlists = glob.glob("namelist_split*.txt")
+    for filePath in split_banlists:
+        with open(filePath, "r") as split_file:
+            num_names += len(split_file.readlines())
+
+    if num_names > 0:
+        start = time.time()
+        for i in range(num_windows):
+            _thread.start_new_thread(browser, ("namelist_split{num}.txt".format(num=i), i, channel))
+            time.sleep(2)
+        while sum(done) < num_windows:
+            print("Progress: {prog:.2%}  Elapsed time: {elapsed}".format(prog=sum(counter) / num_names,
+                                                                         elapsed=str(datetime.timedelta(seconds=int(time.time() - start)))))
+            time.sleep(1)
+        part_files = glob.glob("banned_part*.txt")
+        with open("banned_lists/{streamer}.txt".format(streamer=channel), "a") as banlist:
+            for filePath in part_files:
+                with open(filePath, "r") as part_file:
+                    banlist.write(part_file.read())
+                os.remove(filePath)
+
+
+check_files()
+for chnl in channels:
+    counter = [0] * num_windows
+    done = [False] * num_windows
+    split_files(chnl)
+    start_browsers(chnl)
+    delete_split_namelists()
