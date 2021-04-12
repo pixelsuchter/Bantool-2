f02fdbd60c985e040bd5b25633ef6378876aaf19
pixelsuchter <76219112+pixelsuchter@users.noreply.github.com>
2021-04-12 20:19:07 +0200

    Added user ID export for use with ID based tools

--- CVS_Exporter/CVS_Exporter.py
+++ CVS_Exporter/CVS_Exporter.py
@@ -15,12 +15,13 @@ with open("Followerlist.csv", "r", newline='') as input_file:
         if line != ['userName', 'userID', 'accCreatedAt', 'followCreatedAt']:
             username, userID, create_time, follow_time = line
             if follow_time in follow_dict.keys():
-                follow_dict[follow_time].append(username)
+                follow_dict[follow_time].append((username, userID))
             else:
-                follow_dict[follow_time] = [username]
+                follow_dict[follow_time] = [(username, userID)]
 
-with open("Bots.txt", "w") as output_file:
-    for time, namelist in follow_dict.items():
-        if len(namelist) > 10:
-            for name in namelist:
-                output_file.write(name + "\n")
+with open("Bots.txt", "w") as name_file, open("Bots_IDs.txt", "w") as id_file:
+    for time, name_and_ID_list in follow_dict.items():
+        if len(name_and_ID_list) > 10:
+            for name_id in name_and_ID_list:
+                name_file.write(name_id[0] + "\n")
+                id_file.write(name_id[1] + "\n")
