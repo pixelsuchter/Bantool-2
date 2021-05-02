dd8dc4feda38177815e436026fd4c112f62eeb82
pixelsuchter <76219112+pixelsuchter@users.noreply.github.com>
2021-05-02 12:25:59 +0200

    Users now get flagged as bot if the "Known bot" field is set to 1

--- CVS_Exporter/CSV_Exporter.py
+++ CVS_Exporter/CSV_Exporter.py
@@ -13,15 +13,16 @@ with open("Followerlist.csv", "r", newline='') as input_file:
     input_csv = csv.reader(input_file)
     for line in input_csv:
         if line:
-            if line != ['userName', 'userID', 'accCreatedAt', 'followCreatedAt'] or line != ['userName', 'userID', 'accCreatedAt', 'followCreatedAt', 'isKnownBot']:
+            if line != ['userName', 'userID', 'accCreatedAt', 'followCreatedAt'] and line != ['userName', 'userID', 'accCreatedAt', 'followCreatedAt', 'isKnownBot']:
+                isBot = 0
                 if len(line) == 4:
                     username, userID, create_time, follow_time = line
                 else:
                     username, userID, create_time, follow_time, isBot = line
                 if follow_time in follow_dict.keys():
-                    follow_dict[follow_time].append((username, userID))
+                    follow_dict[follow_time].append((username, userID, isBot))
                 else:
-                    follow_dict[follow_time] = [(username, userID)]
+                    follow_dict[follow_time] = [(username, userID, isBot)]
 
 with open("Bots.txt", "w") as name_file, open("Bots_IDs.txt", "w") as id_file:
     for time, name_and_ID_list in follow_dict.items():
@@ -29,3 +30,8 @@ with open("Bots.txt", "w") as name_file, open("Bots_IDs.txt", "w") as id_file:
             for name_id in name_and_ID_list:
                 name_file.write(name_id[0] + "\n")
                 id_file.write(name_id[1] + "\n")
+        else:
+            for name_id in name_and_ID_list:
+                if name_id[2] == "1":
+                    name_file.write(name_id[0] + "\n")
+                    id_file.write(name_id[1] + "\n")
