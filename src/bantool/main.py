134222975fd7bab2801f9c12820418f84a70744f
Charles Czysz <charles@czysz.io>
2021-08-21 13:47:13 -0500

    Include user list arg

--- src/bantool/main.py
+++ src/bantool/main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-from typing import Optional
+from typing import List, Optional
 from bantool.bantool import Bantool
 from bantool.config import ConfigNT, load_config
+from bantool.utils import sort_file_and_dedupe
 import argparse
-
-# from selenium.common.exceptions import *
 
 import logging
 
@@ -16,36 +15,50 @@ def parse_args(argv: Optional[list] = None) -> ConfigNT:
 
     parser = argparse.ArgumentParser("bantool")
 
-    mutex_group = parser.add_mutually_exclusive_group(required=True)
-    mutex_group.add_argument("--config", help="Path to JSON config.")
+    config_group = parser.add_argument_group("config")
+    config_group.add_argument(
+        "--config", help="Path to JSON config. Values overridden by CLI"
+    )
 
-    cli_group = mutex_group.add_argument_group("CLI")
+    cli_group = parser.add_argument_group()
 
     cli_group.add_argument("--channels", action="append", dest="twitch_channels")
     cli_group.add_argument("--account-name", metavar=str, help="Twitch account name")
-    cli_group.add_argument("--n-browser-windows", metavar=int, dest="Number_of_browser_windows")
-    cli_group.add_argument("--firefox-profile", dest="Firefox_profile", metavar=str, help="Name of new Firefox profile.")
-    cli_group.add_argument("--block", action="store_true", metavar=bool, dest="Block", default=True)
-    cli_group.add_argument("--unblock", action="store_true", metavar=bool, dest="Unblock", default=True)
-    cli_group.add_argument("--ban", action="store_true", metavar=bool, dest="Ban", default=True)
-    cli_group.add_argument("--unban", action="store_true", metavar=bool, dest="Unban", default=True)
+    cli_group.add_argument(
+        "--n-browser-windows", metavar=int, dest="number_of_browser_windows"
+    )
+    cli_group.add_argument(
+        "--firefox-profile",
+        dest="Firefox_profile",
+        metavar=str,
+        help="Name of new Firefox profile.",
+    )
+    cli_group.add_argument("--block", action="store_true", default=True)
+    cli_group.add_argument("--unblock", action="store_true", default=True)
+    cli_group.add_argument("--ban", action="store_true", default=True)
+    cli_group.add_argument("--unban", action="store_true", default=True)
     cli_group.add_argument("--greeting-emote", metavar=str)
     cli_group.add_argument("--chunk-size", metavar=int, default=1000)
-
+    cli_group.add_argument("--namelist", help="List of Twitch accounts, one per line.")
 
     if argv:
-        args, unknown_args = parser.parse_args(argv)
+        args, unknown_args = parser.parse_known_args(argv)
     else:
-        args, unknown_args = parser.parse_args()
+        args, unknown_args = parser.parse_known_args()
+
+    config_dict = {}
     if config := args.config:
-        return load_config(config)
+        config_dict = load_config(config)
 
-    args_dict = {k: v for k, v in vars(args).items() if k in ConfigNT._fields()}
-    return ConfigNT(**args_dict)
+    config_dict.update(dict(vars(args)))
+    return ConfigNT(**{k: v for k, v in config_dict.items() if k in ConfigNT._fields()})
 
 
-def main() -> None:
-    tool = Bantool()
+def main(argv: Optional[List[str]] = None) -> None:
+    args = parse_args(argv)
+    sort_file_and_dedupe(args.namelist)
+
+    tool = Bantool(args)
     tool.run()
 
 
