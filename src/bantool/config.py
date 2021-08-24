4bc8a81082e0416b20fc5e2938dfe37d6df26784
Charles Czysz <charles@czysz.io>
2021-08-24 07:59:53 -0500

    Create default config

--- src/bantool/config.py
+++ src/bantool/config.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+import os.path
 import json
-from typing import List, NamedTuple
+from typing import List, NamedTuple, TypedDict
 import stringcase as sc
+
+
+class ConfigDict(TypedDict):
+    twitch_channels: List[str]
+    account_name: str
+    number_of_browser_windows: int
+    firefox_profile: str
+    block: bool
+    ban: bool
+    unban: bool
+    unblock: bool
+    greeting_emote: str
+    chunk_size: int
+    namelist: str
 
 
 class ConfigNT(NamedTuple):
@@ -19,13 +34,34 @@ class ConfigNT(NamedTuple):
     namelist: str
 
 
-def load_config_to_dict(config_path: str) -> dict:
+default_dict: ConfigDict = dict(
+    twitch_channels=[""],
+    account_name="",
+    number_of_browser_windows=1,
+    firefox_profile="",
+    block=True,
+    ban=True,
+    unban=True,
+    unblock=True,
+    greeting_emote="",
+    chunk_size=1,
+    namelist="",
+)
+
+
+def create_default_config(config_path: str) -> str:
+    with open(config_path, "w") as f:
+        json.dump(default_dict, f)
+    return config_path
+
+
+def load_config_to_dict(config_path: str) -> ConfigDict:
+    if not os.path.exists(config_path):
+        config_path = create_default_config(config_path)
+
     with open(config_path) as cfg:
         config = dict(json.load(cfg))
 
-    processed = {
-        sc.snakecase(k.lower()): v
-        for k, v in config.items()
-    }
+    processed: ConfigDict = {sc.snakecase(k.lower()): v for k, v in config.items()}
 
     return processed
