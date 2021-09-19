7be478452ea1080add57b2e8adb4271764a0da72
Charles Czysz <charles@czysz.io>
2021-09-19 11:39:55 -0500

    Functionalize inits

--- src/bantool/__main__.py
+++ src/bantool/__main__.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python
 
 import click
+import sys
 from bantool.bantool import Bantool
 from bantool.config import download_banlist
 from bantool.config import init_config
 
+import logging
+
+logger = logging.getLogger("bantool")
+
+def init_files():
+    click.echo("Initalizing config files.")
+    init_config("config.json")
+    click.echo("Downloading banlist")
+    download_banlist("namelist.txt")
 
 @click.command()
 @click.option("--init", "-i", "do_init", is_flag=True)
 @click.option("--config", default="config.json", type=str)
 @click.option("--banlist", default="namelist.txt", type=str)
-@click.option()
 def main(do_init, config, banlist):
     if do_init:
-        print("Initalizing config files")
-        init_config(config)
-        print("Downloading banlist")
-        download_banlist(banlist)
-    else:
-        tool = Bantool()
+        init_files()
+        sys.exit(0)
+    try:
+        tool = Bantool(config=config, banlist=banlist)
         tool.run()
+    except Exception as e:
+        logger.exception(e)
+        sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
